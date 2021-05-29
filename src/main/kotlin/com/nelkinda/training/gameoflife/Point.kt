6a3299a6145a7d80debfd7ef15496a6d1823c0e6
Christian Hujer <Christian.Hujer@nelkinda.com>
2021-05-29 15:58:28 +0530

    Fix Kotlin compiler warning about @JvmDefault.

--- src/main/kotlin/com/nelkinda/training/gameoflife/Point.kt
+++ src/main/kotlin/com/nelkinda/training/gameoflife/Point.kt
@@ -4,6 +4,5 @@ interface Point<P : Point<P>> {
     operator fun plus(p: P): P
     operator fun rangeTo(p: P): List<P>
     fun neighbors(): List<P>
-    @JvmDefault
     fun neighbors(predicate: (P) -> Boolean): List<P> = neighbors().filter(predicate)
 }
