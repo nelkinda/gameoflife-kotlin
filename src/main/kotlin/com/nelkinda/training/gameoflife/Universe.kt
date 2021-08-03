9bbc799782446247633b1a53ddef5d9a63c6e6e7
Christian Hujer <Christian.Hujer@nelkinda.com>
2021-08-03 12:43:37 +0530

    Reorder Universe members according to step-down/newspaper rule.

--- src/main/kotlin/com/nelkinda/training/gameoflife/Universe.kt
+++ src/main/kotlin/com/nelkinda/training/gameoflife/Universe.kt
@@ -14,11 +14,11 @@ data class Universe<Cell : Point<Cell>>(
 
     private fun Cell.survives() = rules.survives(countLiveNeighbors())
     private fun Cell.born() = rules.born(countLiveNeighbors())
+    private fun Cell.countLiveNeighbors() = liveNeighbors().count()
+    private fun Cell.liveNeighbors() = neighbors { it.isAlive() }
+    private fun Cell.deadNeighbors() = neighbors { it.isDead() }
     private fun Cell.isAlive() = this in life
     private fun Cell.isDead() = !this.isAlive()
-    private fun Cell.deadNeighbors() = neighbors { it.isDead() }
-    private fun Cell.liveNeighbors() = neighbors { it.isAlive() }
-    private fun Cell.countLiveNeighbors() = liveNeighbors().count()
 
     override fun toString() = "Universe{$rules\n$life}"
 }
