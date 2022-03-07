3229e11f3381b6b233b6b513ad1b83a449c12deb
Christian Hujer <Christian.Hujer@nelkinda.com>
2022-03-07 16:08:15 +0530

    Upgrade Cucumber binding from JUnit 4 RunWith to JUnit 5 Platform Suites.

---
+++ src/test/kotlin/com/nelkinda/training/gameoflife/AcceptanceTest.kt
@@ -0,0 +1,10 @@
+package com.nelkinda.training.gameoflife
+
+import org.junit.platform.suite.api.IncludeEngines
+import org.junit.platform.suite.api.SelectClasspathResource
+import org.junit.platform.suite.api.Suite
+
+@Suite
+@IncludeEngines("cucumber")
+@SelectClasspathResource("features")
+class AcceptanceTest
